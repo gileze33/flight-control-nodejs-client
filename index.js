8211cbd76ad9f29cb9eb4d3786f40d6dbc90264f
Louy Alakkad <louy08@gmail.com>
2015-10-12 15:41:23 +0300

    exit instantly on `uncaughtException`

--- index.js
+++ index.js
@@ -266,17 +266,11 @@ var logger = {
   },
 };
 
-// attach a generic exception handler to write to FlightControl
 process.on('uncaughtException', function(err) {
-  console.trace(err);
-
-  logger.write('error', {
-    type: 'exception',
-    stack: err.stack,
-    error: err,
-  }, function() {
-    process.exit(1);
+  writeLocal('error', {
+    exception: err,
   });
+  process.exit(1);
 });
 
 console.log('Added generic exception handler for FlightControl logger\n');
